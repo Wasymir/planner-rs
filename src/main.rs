4aba1f1e4d5749d78a77262dc26108ab0dd25710
Wasymir <wasymir@gmail.com>
2022-12-23 19:25:01 +0100

    modifed init functions calls in main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+use crate::student::*;
+use crate::subject::*;
 use group::group;
 use student::Student;
 use subject::Subject::*;
-use crate::subject::*;
-use crate::student::*;
 
 #[macro_use]
 extern crate lazy_static;
 
+mod graphs;
 mod group;
 mod student;
 mod subject;
-mod graphs;
 
 pub use graphs::graph::Graph;
 
@@ -18,11 +18,18 @@ fn main() {
     let students = vec![
         Student::new(0, None, vec![BiologySL, ChemistryHL]),
         Student::new(1, None, vec![GermanBHL, PolishALiteratureHL]),
-        Student::new(2, None, vec![MathematicsAAHL, EnglishALanguageAndLiteratureSL]),
-        Student::new(3, None, vec![PolishALiteratureSL, EnglishALanguageAndLiteratureSL]),
+        Student::new(
+            2,
+            None,
+            vec![MathematicsAAHL, EnglishALanguageAndLiteratureSL],
+        ),
+        Student::new(
+            3,
+            None,
+            vec![PolishALiteratureSL, EnglishALanguageAndLiteratureSL],
+        ),
         Student::new(4, None, vec![MathematicsAASL, ChemistrySL]),
     ];
-    init_students(&students);
-    init_subjects();
+    init(&students);
     group();
 }
